f7dcbac5cdfd83141a472a490ce187fd63023b62
Angelus Taylor <angelustaylor@outlook.com>
2022-02-16 16:08:28 -0600

    Got rid of the preloader

--- js/theme.js
+++ js/theme.js
@@ -1,20 +1,5 @@
-/*
-==================================================================================
-* Template:  	 Kenil - Responsive Bootstrap 5 One Page Portfolio Html Template
-* Written by: 	 Harnish Design - (http://www.harnishdesign.net)
-* Description:   Main Custom Script File
-==================================================================================
-*/
-
 (function ($) {
 	"use strict";
-
-// Preloader
-$(window).on('load', function () {
-	$('.lds-ellipsis').fadeOut(); // will first fade out the loading animation
-	$('.preloader').delay(333).fadeOut('slow'); // will fade out the white DIV that covers the website.
-	$('body').delay(333);
-});
 
 
 // Header Sticky
